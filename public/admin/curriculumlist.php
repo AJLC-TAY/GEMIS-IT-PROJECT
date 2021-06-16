dcfbd0a7709981e5f4143a6f7a9bb4b7a73728c8
ajlcutay <2194014@slu.edu.ph>
2021-06-16 15:27:19 +0800

    Fixed search feature, added view button, and refactored javascript

--- public/admin/curriculumlist.php
+++ public/admin/curriculumlist.php
@@ -6,19 +6,25 @@
         width: 275px;
         height: 250px;
     }
+
     .curriculum-con {
         height: 80vh;
     }
+
     .kebab {
         width: 43px;
     }
+
     .kebab::before {
         content: url('../../assets/kebab.svg');
     }
-    .kebab:hover, .kebab:focus {
+
+    .kebab:hover,
+    .kebab:focus {
         background-color: #DCDCDC;
         box-shadow: none;
     }
+
     .spinner-grow {
         position: fixed;
         z-index: 99;
@@ -30,6 +36,7 @@
         bottom: 0;
     }
 </style>
+
 <body>
     <main class="container">
         <!-- HEADER -->
@@ -50,24 +57,31 @@
             <div class="spinner-grow" role="status" hidden>
                 <span class="visually-hidden">Loading...</span>
             </div>
-            <?php require_once ("../../src/getCurriculum.php");
-                foreach($curriculumList as $curr) {
-                    echo "<div id='". $curr->code."-card' class='card shadow-sm'>
+            <!-- No result message -->
+            <div class="msg w-100 d-flex justify-content-center d-none">
+                <p class="m-auto">No results found</p>
+            </div>
+            <?php require_once("../../src/getCurriculum.php");
+            foreach ($curriculumList as $curr) {
+                echo "<div id='" . $curr->code . "-card' class='card shadow-sm p-0'>
                         <div class='card-body'>
                             <div class='dropdown'>
                                 <button class='kebab btn btn-link rounded-circle' data-bs-toggle='dropdown'></button>
                                 <ul class='dropdown-menu'>
-                                    <li><a class='dropdown-item' href='curriculum.php?id=".$curr->code."'>View</a></li>
+                                    <li><a class='dropdown-item' href='curriculum.php?id=" . $curr->code . "'>Edit</a></li>
                                     <li><a class='dropdown-item' href='#'>Archive</a></li>
                                 </ul>
                             </div>
                             <h4>$curr->title</h4>
                             <p>$curr->description</p>
                         </div>
+                        <div class='modal-footer p-0'>
+                            <a role='button' class='btn' href='curriculum.php?id=" . $curr->code . "'>View</a>
+                        </div>
                     </div>";
-                }
+            }
 
-                echo "<div class='btn add-curriculum card shadow-sm'>
+            echo "<div class='btn add-curriculum card shadow-sm'>
                     <div class='card-body'>
                         Add Curriculum
                     </div>
@@ -77,7 +91,7 @@
         <?php include("../footer.html"); ?>
     </main>
 
-    <!-- Modal --> 
+    <!-- Modal -->
     <div class="modal fade" id="addCurriculumModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -110,52 +124,68 @@
 </body>
 
 <script type="text/javascript">
-    var curriculumList = <?php echo json_encode($curriculumList);?>;
+    var curriculumList = <?php echo json_encode($curriculumList); ?>;
     var curriculumCon = $('.curriculum-con')
     var cards = $('.card')
-    var kebab = $('.kebab');
+    var kebab = $('.kebab')
+    var addCurriculumBtn = $('.add-curriculum')
+    var msg = $('.msg')
     var timeout = null;
 
-    $(document).ready(function () {
-        kebab.click(function(){
+    $(document).ready(function() {
+        kebab.click(function() {
             $(this).siblings('.dropdown-menu').show()
         })
 
-        kebab.focusout(function(){
-            $('.dropdown-menu').hide()
-        })
+        kebab.focusout(() => $('.dropdown-menu').hide()) // hides the all dropmenu if the kebab is unfocused
 
-        $('input[type=search]').keyup(function () {
-            clearTimeout(timeout)                   // resets the timer
-            timeout = setTimeout(() => {            // executes the function after the specified milliseconds
-                let input = $(this)
-                var keywords = input.val().trim().toLowerCase()
-                let filterOutCurriculum = (curriculum) => {
+        // $('[data-link]').click(function () {
+        //     window.location.href = $(this).attr('data-link')
+        // })
+
+        function displayResults (results) {
+            if (results.length == 0) {
+                msg.addClass('d-none')
+                cards.each(function () {        // show all curriculum cards if the search bar is empty
+                    $(this).show()      
+                })
+                return
+            } 
+
+            if (results.length == curriculumList.length) {
+                cards.each(function () {
+                    $(this).hide()
+                    addCurriculumBtn.hide()      
+                })
+                msg.removeClass('d-none')
+                return
+            }
+
+            results.forEach(curriculum => {
+                cards.each(function() {
+                    let element = $(this)
+                    if (curriculum.code + '-card' == element.attr('id')) {
+                        element.hide()
+                        addCurriculumBtn.hide()
+                    }
+                })
+            })
+        }
+
+        $('input[type=search]').keyup(function() {
+            clearTimeout(timeout) // resets the timer
+            timeout = setTimeout(() => { // executes the function after the specified milliseconds
+                var keywords = $(this).val().trim().toLowerCase()
+                let filterOutCurriculum = (curriculum) => { // returns the curriculum info that does not contain the keyword
                     return !(curriculum.code.toLowerCase().includes(keywords) || curriculum.description.toLowerCase().includes(keywords) || curriculum.title.toLowerCase().includes(keywords))
                 }
                 var results = curriculumList.filter(filterOutCurriculum)
-                
-                if (results.length == 0) {
-                    cards.each(function () {
-                        $(this).show()
-                    })
-                } else {
-                    results.forEach(curriculum => {
-                        console.log(curriculum)
-                        cards.each(function () {
-                            let element = $(this)
-                            if (curriculum.code + '-card' == element.attr('id')) {
-                                element.hide()
-                            }
-                        })
-                    })
-                }
+                displayResults(results)
             }, 500)
         })
 
-        $('.add-curriculum').click(function() {
-            $('#addCurriculumModal').modal('toggle')
-        })
+        $('.add-curriculum').click(() => $('#addCurriculumModal').modal('toggle'))
     })
 </script>
+
 </html>
