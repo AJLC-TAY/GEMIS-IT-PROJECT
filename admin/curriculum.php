72d6e9cf6c6edaa05ccb5146876d575497dc08ac
Kesleey <2193974@slu.edu.ph>
2021-09-21 14:11:56 +0800

    Prog and curr file structure

--- admin/curriculum.php
+++ admin/curriculum.php
@@ -1,26 +1,6 @@
 <?php
 include_once("../inc/head.html");
-include_once('../class/Administration.php');
-
-$admin = new Administration();
-$curriculum = $admin->getCurriculum();
-$curr_name = $curriculum->get_cur_name();
-$curr_code = $curriculum->get_cur_code();
-$curr_desc = $curriculum->get_cur_desc();
-
-$edit = "disabled";
-$disable_when_edit = "";
-$none_when_edit = "";
-$display_when_edit = "d-none";
-
-if (isset($_GET['state']) && $_GET['state'] == 'edit') {
-    $edit = '';
-    $display_when_edit = "";
-    $disable_when_edit = "disabled";
-    $none_when_edit = "d-none";
-}
-
-?>
+include_once('../class/Administration.php');?>
 
 <title>Curriculum | GEMIS</title>
 <link href='../assets/css/bootstrap-table.min.css' rel='stylesheet'>
@@ -42,93 +22,16 @@ if (isset($_GET['state']) && $_GET['state'] == 'edit') {
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="row ps-3">
-                            <!-- HEADER -->
-                            <header>
-                                <!-- BREADCRUMB -->
-                                <nav aria-label="breadcrumb">
-                                    <ol class="breadcrumb">
-                                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
-                                        <li class="breadcrumb-item"><a href="curriculumList.php">Curriculum</a></li>
-                                        <li class="breadcrumb-item active" aria-current="page"><?php echo $curr_name; ?></li>
-                                    </ol>
-                                </nav>
-                                <h2 class="fw-bold"><?php echo $curr_name; ?></h2>
-                                <hr class="my-2">
-                                <h6 class="fw-bold">Curriculum</h6>
-                            </header>
-                            <!-- HEADER END -->
-                            <!-- FORM -->
-                            <div class="currcard container">
-                                <div class='card'>
-                                    <div class="d-flex justify-content-between">
-                                        <h4>Information</h4>
-                                        <div class="btn-con my-a">
-                                            <input type="hidden" name="action" value="updateCurriculum">
-                                            <button id='edit-btn' class='btn link btn-sm <?php echo $none_when_edit; ?>'><i class="bi bi-pencil-square me-2"></i>Edit</button>
-                                            <div class="decide-con <?php echo $display_when_edit; ?>">
-                                                <a id="cancel-btn" href='curriculum.php?code=<?php echo $curr_code; ?>' class="btn btn-dark btn-sm me-1">Cancel</a>
-                                                <!-- <button id="cancel-btn" class="btn btn-dark btn-sm me-1 <?php echo $display_when_edit; ?>">Cancel</button> -->
-                                                <input type="submit" form="curriculum-form" class="btn btn-success btn-sm" value="Save">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <hr class='mt-2 mb-4'>
-                                    <section class="w-100">
-                                        <div class="ps-3 row w-100">
-                                            <form id='curriculum-form' class="needs-validation" action="action.php" method="POST" novalidate>
-                                                <div class="form-group row">
-                                                    <div class="col-sm-3"><label class="my-auto">Code</label></div>
-                                                    <div class="col-sm-9">
-                                                        <input type="hidden" name="action" value="updateCurriculum">
-                                                        <input type="hidden" name="current_code" value="<?php echo $curr_code; ?>">
-                                                        <?php echo "<input class='form-control form-input ' type='text' name='code' value='$curr_code' $edit required>
-                                                                    <div class='invalid-input'>
-                                                                        Please enter curriculum code
-                                                                    </div>"; ?>
-                                                    </div>
-                                                    <label class="col-sm-3">Name</label>
-                                                    <div class="col-sm-9">
-                                                        <?php echo "<input class='form-input form-control ' type='text' name='name' value='$curr_name' $edit required>
-                                                                    <div class='invalid-input'>
-                                                                        Please entercurriculum name
-                                                                    </div>"; ?>
-                                                    </div>
-                                                    <label class="col-sm-3">Description</label>
-                                                    <div class="col-sm-9">
-                                                        <?php echo "<textarea  class='form-input form-control ' name='curriculum-desc' $edit>" . $curr_desc . "</textarea>"; ?>
-                                                    </div>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </section>
-                                </div>
-                            </div>
-                            <!-- FORM END -->
-                            <!-- STRAND TABLE -->
-                            <div class="container mt-5">
-                                <div class="card w-100 h-auto">
-                                    <table id="table" class="table-striped table-sm">
-                                        <thead class='thead-dark track-table'>
-                                            <div class="d-flex justify-content-between align-items-center mb-3">
-                                                <h5 class="fw-bold">STRAND LIST</h5>
-                                                <a href="#add-modal" id="add-btn" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#add-modal"><i class="bi bi-plus me-2"></i>Add strand</a>
-                                            </div>
-                                            <hr class="mt-1 mb-4">
-                                            <div class="d-flex flex-row-reverse mb-3">
-                                                <!-- <div class="d-flex mb-3"> -->
-                                                <button id="track-archive-btn" class="btn btn-secondary btn-sm"><i class="bi bi-archive me-2"></i>Archive</button>
-                                            </div>
-                                            <tr>
-                                                <th data-checkbox="true"></th>
-                                                <th scope='col' data-width="100" data-align="center" data-field='prog_code'>Code</th>
-                                                <th scope='col' data-width="600" data-align="center" data-sortable="true" data-field="prog_desc">Program/Strand Description</th>
-                                                <th scope='col' data-width="300" data-align="center" data-field="action">Actions</th>
-                                            </tr>
-                                        </thead>
-                                    </table>
-                                </div>
-                            </div>
-                            <!-- STRAND TABLE END -->
+                            <?php
+                            if (isset($_GET['code'])){
+                                include_once("curriculum/curriculumView.php"); 
+                                $jsFilePath = "../js/admin/program.js";
+                            } else {
+                                include_once("curriculum/curriculumCards.php"); 
+                                $jsFilePath = "../js/admin/curriculum-card.js";
+                            }
+                            ?>
+                            
                         </div>
                     </div>
                 </div>
@@ -137,82 +40,12 @@ if (isset($_GET['state']) && $_GET['state'] == 'edit') {
             <?php include_once("../inc/footer.html"); ?>
             <!-- FOOTER END -->
         </section>
-        <!-- MAIN CONTENT END-->
-        <!-- ARCHIVE CONFIRMATION MODAL -->
-        <div class="modal fade" id="track-archive-modal" tabindex="-1" aria-labelledby="modal confirmation msg" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <div class="modal-title">
-                            <h4 class="mb-0">Confirmation</h4>
-                        </div>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <h5>Do you want to archive <span id="modal-identifier"></span>?</h5>
-                        <p class="modal-msg"></p>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="close btn btn-secondary close-btn" data-bs-dismiss="modal">Close</button>
-                        <button class="btn btn-primary close-btn archive-btn">Archive</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- ARCHIVE CONFIRMATION MODAL END -->
-        <!-- ADD MODAL -->
-        <div class="modal fade" id="add-modal" tabindex="-1" aria-labelledby="modal addProgram" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <div class="modal-title">
-                            <h4 class="mb-0">Add Strand/Program</h4>
-                        </div>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form id="prog-form" action="">
-                            <div class="form-group needs-validation" novalidate>
-                                <label for="prog-code">Strand Code</label>
-                                <input id="prog-code" type="text" name="code" class='form-control' placeholder="Enter unique code here. ex. STEM" required>
-                                <div class="invalid-input">
-                                    Please enter a unique code
-                                </div>
-                                <p class="unique-error-msg text-danger m-0 invisible"><small>Please provide a unique strand code</small></p>
-                                <label for="prog-name">Strand Name</label>
-                                <input id="prog-name" type="text" name="desc" class='form-control' placeholder="ex. Science, Technology, Engineering, and Math" required>
-                                <div class="invalid-input">
-                                    Please enter description
-                                </div>
-                                <p class="name-error-msg text-danger m-0 invisible"><small>Please provide the program name</small></p>
-                                <label for="prog-curr">Curriculum</label>
-                                <input type="text" class='form-control' name="curr-code" value="<?php echo ($curr_code); ?>" readonly>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="close btn btn-secondary close-btn" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" name="submit-prog" form="prog-form" class="submit btn btn-primary" data-link='addProg.php'>Add</button>
-                    </div>
-                </div>
-            </div>
-        </div>
     </section>
-    <!-- ADD MODAL END -->
     <!-- TOAST -->
     <div aria-live="polite" aria-atomic="true" class="position-relative" style="bottom: 0px; right: 0px">
         <div id="toast-con" class="position-fixed d-flex flex-column-reverse overflow-visible " style="z-index: 99999; bottom: 20px; right: 25px;"></div>
     </div>
     <!-- TOAST END -->
-    <!-- JQUERY FOR BOOTSTRAP TABLE -->
-    <script src="../assets/js/bootstrap-table.min.js"></script>
-    <script src="../assets/js/bootstrap-table-en-US.min.js"></script>
-
-    <script type="text/javascript">
-        var code = <?php echo json_encode($curr_code); ?>;
-    </script>
-    <script type="text/javascript" src="../js/common-custom.js"></script>
-    <script type="module" src="../js/admin/curriculum.js"></script>
 </body>
 <!-- VALIDATION -->
 <script>
@@ -233,5 +66,7 @@ if (isset($_GET['state']) && $_GET['state'] == 'edit') {
 <!-- JQUERY FOR BOOTSTRAP TABLE -->
 <script src="../assets/js/bootstrap-table.min.js"></script>
 <script src="../assets/js/bootstrap-table-en-US.min.js"></script>
+<script type="text/javascript" src="../js/common-custom.js"></script>
+<script type="module" src="<?php echo $jsFilePath; ?>"></script>
 
 </html>
---
+++ admin/program/programView.php
@@ -0,0 +1,124 @@
+<?php include_once("../inc/head.html");
+require_once('../class/Administration.php');
+$admin = new Administration();
+$program = $admin->getProgram();
+$prog_name = $program->get_prog_desc();
+$prog_code = $program->get_prog_code();
+$prog_curr_code = $program->get_curr_code();
+$state = "disabled";
+$edit_btn_state = "";
+$display = "d-none";
+if (isset($_GET['state']) && $_GET['state'] == 'edit') {
+    $state = '';
+    $edit_btn_state = "disabled";
+    $display = "";
+    $edit_btn_display = "d-none";
+}
+?>
+
+
+<header>
+    <!-- BREADCRUMB -->
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+            <li class="breadcrumb-item"><a href="program.php">Programs</a></li>
+            <li class="breadcrumb-item active" aria-current="page"><?php echo $prog_name; ?></li>
+        </ol>
+    </nav>
+    <h2><?php //echo $prog_name;
+        ?></h2>
+    <h5><?php //echo $prog_curr_code;
+        ?></h5>
+    <!-- <hr> -->
+    <h2><?php echo $prog_name; ?></h2>
+    <h5><?php echo $prog_curr_code; ?></h5>
+</header>
+
+<!-- Form -->
+<div class="container mt-4">
+    <div class="card w-100 h-auto">
+        <div class="d-flex justify-content-between">
+            <h4>Information</h4>
+            <div class="btn-con my-a">
+                <button id='edit-btn' class='btn link btn-sm <?php echo $edit_btn_display; ?>'><i class="bi bi-pencil-square me-2"></i>Edit</button>
+                <div class="decide-con <?php echo $display; ?>">
+                    <a id="cancel-btn" href='program.php?prog_code=<?php echo $prog_code; ?>' class="btn btn-dark btn-sm me-1">Cancel</a>
+                    <!--                                                <button id="cancel-btn" class="btn btn-dark btn-sm me-1">Cancel</button>-->
+                    <input type="submit" form='program-view-form' class="btn btn-success btn-sm" value="Save">
+                </div>
+            </div>
+        </div>
+        <hr class='mt-2 mb-4'>
+        <form id='program-view-form' class="needs-validation" method="POST" novalidate>
+            <input type="hidden" name="action" value="updateProgram">
+            <div class="form-group row">
+                <label class="col-xl-2 col-lg-3 col-form-label text-start">Program Code</label>
+                <div class="col-xl-10 col-lg-9">
+                    <input type="hidden" name="current_code" value="<?php echo $prog_code; ?>">
+                    <?php echo "<input class='form-input form-control' type='text' name='code' value='$prog_code' $state required> 
+                                                <div class='invalid-feedback'>
+                                                    Please enter current code
+                                                </div>"; ?>
+                </div>
+                <label class="col-xl-2 col-lg-3 col-form-label text-start">Description</label>
+                <div class='col-xl-10 col-lg-9'>
+                    <!-- <input name="name" value="<?php //echo $prog_name; 
+                                                    ?>" disabled> -->
+                    <?php echo "<textarea class='form-input form-control' name='name' $state>" . $prog_name . "</textarea>"; ?>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<!-- Subject table -->
+<div class="container mt-5">
+    <div class="card w-100 h-auto">
+        <table id="table" class="table-striped table-sm">
+            <thead class='thead-dark track-table'>
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h5 class="fw-bold">SUBJECTS</h5>
+                    <a href="subject.php?prog_code=<?php echo $prog_code; ?>&action=add" id="add-btn" class="btn btn-success"><i class="bi bi-plus me-2"></i>Add subject</a>
+                </div>
+                <hr class="mt-1 mb-4">
+                <div class="d-flex flex-row-reverse mb-3">
+                    <!-- <div class="d-flex mb-3"> -->
+                    <button id="subject-archive-btn" class="btn btn-secondary btn-sm"><i class="bi bi-archive me-2"></i>Archive</button>
+                </div>
+                <tr>
+                    <th data-checkbox="true"></th>
+                    <th scope='col' data-width="100" data-align="right" data-field='sub_code'>Code</th>
+                    <th scope='col' data-width="600" data-sortable="true" data-field="sub_name">Subject Name</th>
+                    <th scope='col' data-width="100" data-sortable="true" data-field="sub_type">Subject Type</th>
+                    <th scope='col' data-width="300" data-align="center" data-field="action">Actions</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
+
+
+<!-- Archive subject confirmation -->
+<div class="modal fade" id="subject-archive-modal" tabindex="-1" aria-labelledby="modal confirmation msg" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="modal-title">
+                    <h4 class="mb-0">Confirmation</h4>
+                </div>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h5>Do you want to archive <span id="modal-identifier"></span>?</h5>
+                <p class="modal-msg"></p>
+            </div>
+            <div class="modal-footer">
+                <button class="close btn btn-secondary close-btn" data-bs-dismiss="modal">Close</button>
+                <button class="btn btn-primary close-btn archive-btn">Archive</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    var code = <?php echo json_encode($prog_code); ?>;
+</script>
