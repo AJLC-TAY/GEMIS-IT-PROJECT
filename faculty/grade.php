9a72f28373002c1b9329417708a9c6c88097d417
Caryl2192339 <2192339@slu.edu.ph>
2021-10-19 23:44:05 +0800

    Faculty Module Grade UI Improvement

--- faculty/grade.php
+++ faculty/grade.php
@@ -23,7 +23,7 @@ $sub_table_display = '';
 $schoolYearInfo = $faculty->getSchoolYearInfo($sy_id); //to be removed pag maayos ung sa session
 $sem = $schoolYearInfo['sem'] == '1' ? 'First' : 'Second';
 $grading = $schoolYearInfo['grading'] == '1' ? 'First' : 'Second';
-$qtrs = $schoolYearInfo['sem'] == '1' ? ['1st','2nd']  : ['3rd','4th'];
+$qtrs = $schoolYearInfo['sem'] == '1' ? ['1st', '2nd']  : ['3rd', '4th'];
 
 
 if (count($sub_classes) != 0) {
@@ -52,7 +52,7 @@ if (count($sub_classes) != 0) {
 </head>
 
 <body>
-    
+
     <!-- SPINNER -->
     <div id="main-spinner-con" class="spinner-con">
         <div id="main-spinner-border" class="spinner-border" role="status">
@@ -77,54 +77,52 @@ if (count($sub_classes) != 0) {
                                         <li class="breadcrumb-item active">Grade</li>
                                     </ol>
                                 </nav>
-                                <div class="row align-content-center">
-                                    <div class="col-auto">
-                                        <h3 class="fw-bold" id='class'></h3>
-                                        <h4><?php echo $sem ?> Semester & <?php echo $grading ?> Quarter</h4>
+                                <div class="d-flex justify-content-between mb-3">
+                                    <h3 class="fw-bold" id='class'></h3>
+                                    <div>
+                                        <button type="button" class="btn btn-outline-primary"><i class="bi bi-eye me-2"></i>Download Template</button>
                                     </div>
-
-                                </div>
                             </header>
                             <!-- STUDENTS TABLE -->
-                            <div class="container mt-1 w-75 ms-0">
-                                <div class="card w-100 h-auto bg-light" style="min-height: 70vh !important;">
 
-                                    <div class="d-flex justify-content-between mb-3">
+                            <div class="container mt-1 ms-0">
+                                <div class="card w-100 h-auto bg-light" style="min-height: 70vh !important;">
+                                    <h5 class="fw-bold"><?php echo $sem ?> Semester & <?php echo $grading ?> Quarter</h5>
+                                    <div class="d-flex justify-content-between mb-1">
                                         <!-- SEARCH BAR -->
                                         <div class="flex-grow-1 me-3">
                                             <input id="search-input" type="search" class="form-control" placeholder="Search something here">
                                         </div>
-                                        <div class="col-auto" style="min-width: 250px !important;">
-                                            <select name="" class="form-control form-control-sm mb-3 w-auto" id="classes">
+                                        <div class="w-25">
+                                            <select class="form-select form-select-sm" id="classes">
                                                 <?php
                                                 echo $sub_class_opn;
                                                 ?>
                                             </select>
                                         </div>
-
+                                        <div>
+                                            <form method="post" action="action.php" class="ms-2">
+                                                <input class='hidden' id='export_code' name='code' value=''>
+                                                <input type="submit" name="export" value="Export">
+                                            </form>
+                                        </div>
+                                        <div>
+                                            <!-- <form method="post" action="action.php"><input type="submit" id='export' name="export" class="btn btn-secondary" value="EXPORT"></form> -->
+                                            <!-- <button type="submit" class="btn btn-secondary export" >EXPORT</button>
+                                            <button onclick="Export()" class="btn btn-secondary">EXPORT</button> -->
+                                            <button type="button" class="btn btn-secondary ms-2"><i class="bi bi-box-arrow-down-left me-2"></i>Import</button>
+                                            <button type="button" class="btn btn-success confirm">Submit</button>
+                                        </div>
                                     </div>
 
-                                    <div class>
-                                        <button type="button" class="btn btn-secondary">Template</button>
-                                        <button type="button" class="btn btn-secondary">Import</button>
-                                        <form method="post" action="action.php">
-                                            <input class = 'hidden' id = 'export_code' name = 'code' value = ''>
-                                            <input type="submit" name="export" value="EXPORT"></form>
-                                        <!-- <form method="post" action="action.php"><input type="submit" id='export' name="export" class="btn btn-secondary" value="EXPORT"></form> -->
-                                        <!-- <button type="submit" class="btn btn-secondary export" >EXPORT</button>
-                                        <button onclick="Export()" class="btn btn-secondary">EXPORT</button> -->
-                                        <button type="button" class="btn btn-success confirm" >SUBMIT</button>
-
-
-                                    </div>
-                                    <form id= 'grades'>
+                                    <form id='grades'>
                                         <table id="table" class="table-striped table-sm">
                                             <thead class='thead-dark'>
                                                 <tr>
                                                     <th scope='col' data-width="150" data-align="center" data-field="stud_id"></th>
                                                     <th scope='col' data-width="300" data-halign="center" data-align="left" data-sortable="true" data-field="name">Student Name</th>
-                                                    <th scope='col' data-width="100" data-align="center" data-sortable="true" contenteditable="true" data-field="grd_1"><?php echo $qtrs[0];?> Quarter</th>
-                                                    <th scope='col' data-width="100" data-align="center" data-sortable="true" data-field="grd_2"><?php echo $qtrs[1];?> Quarter</th>
+                                                    <th scope='col' data-width="100" data-align="center" data-sortable="true" contenteditable="true" data-field="grd_1"><?php echo $qtrs[0]; ?> Quarter</th>
+                                                    <th scope='col' data-width="100" data-align="center" data-sortable="true" data-field="grd_2"><?php echo $qtrs[1]; ?> Quarter</th>
                                                     <th scope='col' data-width="100" data-align="center" data-sortable="true" data-field="grd_f">Final Grade</th>
                                                 </tr>
                                             </thead>
@@ -136,8 +134,8 @@ if (count($sub_classes) != 0) {
                         </div>
                     </div>
                 </div>
-    
-                
+
+
                 <!-- FOOTER START -->
                 <?php include_once("../inc/footer.html"); ?>
                 <!-- FOOTER END -->
@@ -145,26 +143,26 @@ if (count($sub_classes) != 0) {
         </section>
     </section>
     <div class="modal fade grading-confirmation" tabindex="-1" aria-labelledby="modal confirmation msg" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <div class="modal-title">
-                    <h4 class="mb-0">Are you sure you want to submit?</h4>
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <div class="modal-title">
+                        <h4 class="mb-0">Are you sure you want to submit?</h4>
+                    </div>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                
-                <p class="modal-msg">Submitted grades are editable within the duration of the current quarter.</p>
-            </div>
-            <div class="modal-footer">
-                <button class="close btn btn-secondary close-btn" data-bs-dismiss="modal">Cancel</button>
-                <button class="btn close-btn btn-success submit">Submit</button>
+                <div class="modal-body">
+
+                    <p class="modal-msg">Submitted grades are editable within the duration of the current quarter.</p>
+                </div>
+                <div class="modal-footer">
+                    <button class="close btn btn-secondary close-btn" data-bs-dismiss="modal">Cancel</button>
+                    <button class="btn close-btn btn-success submit">Submit</button>
+                </div>
             </div>
         </div>
     </div>
-</div>
-    
+
     <!-- MAIN CONTENT END -->
     <!-- TOAST -->
     <div aria-live="polite" aria-atomic="true" class="position-relative" style="bottom: 0; right: 0;">
