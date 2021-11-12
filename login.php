947a621fb5f82f31e4dcaf9880f6892c011b97f9
Kesleey <2193974@slu.edu.ph>
2021-11-12 22:13:06 +0800

    login, enrollment dashboard ui updated

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ $enroll = $sy[0];
 </head>
 
 <body>
-    <div class="container h-100">
+    <div class="container">
         <div class="login_name_wrapper mb-5">
             <div class="d-flex justify-content-center">Welcome!</div>
         </div>
@@ -33,7 +33,7 @@ $enroll = $sy[0];
                     <form action="inc/authenticate.php" method="post" style="width: 320px">
                         <div class="input-group mb-3">
                             <div class="input-group-prepend">
-                                <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16"  fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                                         <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                                     </svg></span>
@@ -42,7 +42,7 @@ $enroll = $sy[0];
                         </div>
                         <div class="input-group mb-4">
                             <div class="input-group-prepend">
-                                <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
+                                <span class="input-group-text" id="basic-addon1"><svg xmlns="http://www.w3.org/2000/svg" width="16"  fill="currentColor" class="bi bi-key-fill" viewBox="0 0 16 16">
                                         <path d="M3.5 11.5a3.5 3.5 0 1 1 3.163-5H14L15.5 8 14 9.5l-1-1-1 1-1-1-1 1-1-1-1 1H6.663a3.5 3.5 0 0 1-3.163 2zM2.5 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
                                     </svg></span>
                             </div>
