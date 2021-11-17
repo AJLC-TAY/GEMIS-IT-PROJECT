c1c8aa00969a6216ca71afcac2fd9691888b215b
Cygwen <2190485@slu.edu.ph>
2021-11-17 23:02:02 +0800

    Added margins per data list in student profile

--- admin/student/studentProfile.php
+++ admin/student/studentProfile.php
@@ -195,7 +195,7 @@ switch ($user_type) {
                                 $birthdate = date("F j, Y", strtotime($birthdate));
                                 $name = $userProfile->get_name();
                                 echo 
-                                "<dl class='row mb-3'>
+                                "<dl class='row mb-3 ms-2'>
                                             <dt class='col-md-3'>Name: </dt>
                                             <dd class='col-md-9'> $name </dd>
                                             <dt class='col-md-3'>Gender: </dt>
@@ -219,7 +219,7 @@ switch ($user_type) {
                             <div class="row mt-3">
                                 <h6><b>Contact Information</b></h6>
                                 <?php echo 
-                                    "<dl class='row mb-3'>
+                                    "<dl class='row mb-3 ms-2'>
                                         <dt class='col-md-3'>Home Address: </dt>
                                         <dd class='col-md-9'> $add </dd>
                                         <dt class='col-md-3'>Cellphone No.: </dt>
@@ -238,7 +238,7 @@ switch ($user_type) {
                                         $occupation = ${$parent . '_occupation'};
                                         $no = ${$parent . '_cp_no'};
                                         echo "
-                                        <dl class='row mb-3'>
+                                        <dl class='row mb-3 ms-2'>
                                             <dt class='col-md-3'>". ucwords($parent) ."'s Name: </dt>
                                             <dd class='col-md-9'> $name </dd>
                                             <dt class='col-md-3'>Occupation: </dt>
@@ -250,7 +250,7 @@ switch ($user_type) {
                                 }
                                 if ($guardian != NULL) {
                                     echo "<h6 class='mt-3'>Guardian/s</h6>
-                                        <dl class='row mb-3'>
+                                        <dl class='row mb-3 ms-2'>
                                             <dt class='col-md-3'>Guardian's Name: </dt>
                                             <dd class='col-md-9'> $guardian_name </dd>
                                             <dt class='col-md-3'>Relationship: </dt>
